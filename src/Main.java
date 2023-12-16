f5097246cfa9a99df9b57be78711e273b35cb889
Aytan Novruzlu <aytannovruzlu@Aytans-MacBook-Air-4.local>
2023-12-16 23:42:34 +0400

    code cleanup

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        //Service test1 = new Service();
-        //test1.start();
         View view = new ConsoleUI();
         view.start();
     }
--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,18 +1,16 @@
 package presenter;
 
-// import java.nio.channels.AcceptPendingException;
 import java.time.LocalDate;
 
 import view.View;
 import model.user.Gender;
-// import model.Service;
+
 import model.bank.AccountManager;
 import model.file_handler.FileHandler;
 import model.file_handler.SaveToFile;
 
 public class Presenter {
     private View view;
-    // private Service service;
     private AccountManager accountManager;
     private SaveToFile fileHandler;
 

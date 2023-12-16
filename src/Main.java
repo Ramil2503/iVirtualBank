d7bc6864bfb9c8d91a7c0b0fdaaa85468df971e8
Nuraya <neminli15540@ada.edu.az>
2023-12-16 23:38:52 +0400

    .

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-//import model.Service;
+
 
 import view.ConsoleUI;
 import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        //Service test1 = new Service();
-        //test1.start();
         View view = new ConsoleUI();
         view.start();
     }

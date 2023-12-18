a788fa0064b5b4d009b5275e181a932b84fc38d3
Ramil2503 <ramil.ax2503@gmail.com>
2023-12-18 12:39:52 +0400

    code cleanup

--- src/model/user/User.java
+++ src/model/user/User.java
@@ -1,65 +1,65 @@
-// package model.user;
+package model.user;
 
-// import java.time.LocalDate;
+import java.time.LocalDate;
 
-// public class User {
-//     private long id;
-//     private String name;
-//     private String surname;
-//     private Gender gender;
-//     private LocalDate birthDate;
+public class User {
+    private long id;
+    private String name;
+    private String surname;
+    private Gender gender;
+    private LocalDate birthDate;
 
-//     public User (long id, String name, String surname, Gender gender, LocalDate birthDate) {
-//         this.id = id;
-//         this.name = name;
-//         this.surname = surname;
-//         this.gender = gender;
-//         this.birthDate = birthDate;
-//     }
+    public User (long id, String name, String surname, Gender gender, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.birthDate = birthDate;
+    }
 
-//     public long getId() {
-//         return id;
-//     }
+    public long getId() {
+        return id;
+    }
 
-//     public void setId(long id) {
-//         this.id = id;
-//     }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-//     public String getName() {
-//         return name;
-//     }
+    public String getName() {
+        return name;
+    }
 
-//     public void setName(String name) {
-//         this.name = name;
-//     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-//     public String getSurname() {
-//         return surname;
-//     }
+    public String getSurname() {
+        return surname;
+    }
 
-//     public void setSurname(String surname) {
-//         this.surname = surname;
-//     }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
-//     public Gender getGender() {
-//         return gender;
-//     }
+    public Gender getGender() {
+        return gender;
+    }
 
-//     public void setGender(Gender gender) {
-//         this.gender = gender;
-//     }
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 
-//     public LocalDate getBirthDate() {
-//         return birthDate;
-//     }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
 
-//     public void setBirthDate(LocalDate birthDate) {
-//         this.birthDate = birthDate;
-//     }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 
-//     @Override
-//     public String toString() {
-//         return "User [id=" + id + ", name=" + name + ", surname=" + surname + ", gender=" + gender + ", birthDate="
-//                 + birthDate + "]";
-//     }
-// }
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", surname=" + surname + ", gender=" + gender + ", birthDate="
+                + birthDate + "]";
+    }
+}
